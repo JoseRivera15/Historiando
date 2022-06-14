658fd7d1f0b64a17ed7c635aebefc7cc9c362146
JoseRivera15 <107453147+JoseRivera15@users.noreply.github.com>
2022-06-13 23:00:49 -0500

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,113 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Entre que rios se fundo Mesopotamia?",
+    answer: "Rio tigris y eufrates",
+    options: [
+      "Rio nilo y Eufrates",
+      "Rio tigris y eufrates",
+      "Rio nilo y tigris",
+      "Rio indo y nilo"
+    ]
+  },
+    {
+    numb: 2,
+    question: "En que siglo se unifico Egipto?",
+    answer: "3200 a.C.",
+    options: [
+      "3500 a.C.",
+      "3000 a.C.",
+      "3200 a.C.",
+      "3300 a.C."
+    ]
+  },
+    {
+    numb: 3,
+    question: "Que significa democracia?",
+    answer: "Gobierno del pueblo",
+    options: [
+      "Gobierno del pueblo",
+      "Elegido por el pueblo",
+      "El pueblo gobierna",
+      "Ninguna de las anteriores"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Que ciudad goberno Alejandro Magno?",
+    answer: "Macedonia",
+    options: [
+      "Atenas",
+      "Egipto",
+      "Macedonia",
+      "Creta"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Cual fue el puerto mas importante en Egipto?",
+    answer: "Alejandria",
+    options: [
+      "Alejandria",
+      "Damieta",
+      "Puerto Said",
+      "Marsa Matruh"
+    ]
+  },
+    {
+    numb: 6,
+    question: "¿Dónde tuvo lugar el origen del ser humano?",
+    answer: "África",
+    options: [
+       "África",
+       "Asia",
+       "Antártida",
+       "Europa"
+       ]
+     },
+     {
+      numb: 7,
+      question: "¿Cómo se nombró a los homínidos antes de los Homo?",
+      answer: "Australopithecus",
+      options: [
+         "Habilis",
+         "Erectus",
+         "Australopithecus",
+         "Neardenthalesis"
+         ]
+       },
+       {
+        numb: 8,
+        question: "¿Qué material se utilizaba para la creación de herramientas?",
+        answer: "Sílex",
+        options: [
+           "Plástico",
+           "Metal",
+           "Agua",
+           "Sílex"
+           ]
+         },
+         {
+          numb: 9,
+          question: "Cuál fue el primer continente al que migraron los homínidos de África?",
+          answer: "Asia",
+          options: [
+             "Europa",
+             "Asia",
+             "Oceanía",
+             "América"
+             ]
+           },
+           {
+            numb: 10,
+            question: "¿En qué año comenzaron las glaciaciones?",
+            answer: "80,000-10,500 a.C.",
+            options: [
+               "20,000-15,000 a.C.",
+               "80,000-10,500 a.C.",
+               "100,000-70,000 a.C.",
+               "200-50 a.C."
+               ]
+             },
+];
